a8ed5e9d2b3059e3d5efda0b5cbf87a7e23be2ff
Tom Kerr <tomhammers@googlemail.com>
2015-11-10 22:40:33 +0000

    Setting up some more node stuff

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-var express = require('express');
-var app = express();
+var http = require('http'); // add http module
 
-app.listen(3000);
+var myServer = http.createServer(function(request, response) {
+	response.writeHead(200, {"Content-Type" : "text/html"});
+	response.write("<b>Hello</b>");
+	response.end();
+});// create a server
+
+myServer.listen(3000);
+
+
