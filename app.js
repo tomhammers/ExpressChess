d0ff1926d386d6dbc96f8275d281dc4cb41557c4
Tom Kerr <Tom Kerr>
2015-11-11 22:21:37 +0000

    initial chessboard drawn

--- app.js
+++ app.js
@@ -6,22 +6,26 @@ if (canvas.getContext) {
 	// unsupported canvas
 }
 
-var width = canvas.scrollWidth; // responsive width
-var height = canvas.scrollHeight; // responsive height
+var width = canvas.width; // responsive width
+var height = canvas.height; // responsive height
 var rows = 8;
 var columns = 8;
-var blah = 0;
+
 
 var widthIncrement = width / columns;
+var heightIncrement = height / rows;
 
+for(var i = 0; i < 8; i++) {
+	for(var j = 0; j < 8; j++) {
+		context.strokeRect(i * widthIncrement, j * heightIncrement, widthIncrement, heightIncrement);
+		context.stroke();
 
-for(var i = 0; i < rows; i++) {
-	
-	context.strokeRect(blah+0.5, 0+0.5, widthIncrement, 20);
-	//console.log(i * widthIncrement);
-	console.log(i * widthIncrement);
-	blah +=20;
+		console.log(i * widthIncrement);
+		console.log(width);
+	}
 }
-context.stroke();
 
-canvas.style.border   = "1px solid";
+
+
+
+//canvas.style.border   = "1px solid";
