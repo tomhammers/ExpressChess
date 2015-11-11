7431c766d2d0c5e79ef77f51d2200420abe8f5c3
Tom Kerr <tomhammers@googlemail.com>
2015-11-11 16:33:31 +0000

    start of chessboard attempt

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-var http = require('http'); // add http module
 
-var myServer = http.createServer(function(request, response) {
-	response.writeHead(200, {"Content-Type" : "text/html"});
-	response.write("<b>Hello</b>");
-	response.end();
-});// create a server
+var canvas = document.getElementById('game'); 
+if (canvas.getContext) {
+	var context = canvas.getContext('2d');
+} else {
+	// unsupported canvas
+}
 
-myServer.listen(3000);
+var width = canvas.scrollWidth; // responsive width
+var height = canvas.scrollHeight; // responsive height
+var rows = 8;
+var columns = 8;
+var blah = 0;
+
+var widthIncrement = width / columns;
 
 
+for(var i = 0; i < rows; i++) {
+	
+	context.strokeRect(blah+0.5, 0+0.5, widthIncrement, 20);
+	//console.log(i * widthIncrement);
+	console.log(i * widthIncrement);
+	blah +=20;
+}
+context.stroke();
+
+canvas.style.border   = "1px solid";
