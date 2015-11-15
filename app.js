1c9b5ea57cb2aee65a32d8ee152d57f7d7e3f872
Tom Kerr <tomhammers@googlemail.com>
2015-11-15 21:34:22 +0000

    clean chessboard drawn and rendering first chess piece

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-var ChessBoard = function ChessBoard(canvas) {
+"use strict";
 
+$(document).ready(function() {
+
+var ChessBoard = function ChessBoard(canvas) {
 	this.canvas = canvas; 
 	this.context = this.canvas.getContext('2d');
 	this.width = canvas.width;
@@ -15,15 +18,16 @@ var ChessBoard = function ChessBoard(canvas) {
 			for(var column = 0; column < 8; column++) {
 				if(this.alternativeSquare(row,column)) {
 					// only actually need to fill in alternative squares
-					this.colourSquare('#c9c9c9', row * this.widthIncrement, column * this.heightIncrement)
-					// if (row === 0 || row ===1) {
-					// 	context.fillText(265A, row, column);
-					// }
+					this.colourSquare('#C5C8C9', row * this.widthIncrement, column * this.heightIncrement)
+
+					//var kingB = new ChessPiece('../img/kingB.svg');
+				
+					
+					
 				}
-				// pseudo else, if not an alternative square, just leave it as white
+				// pseudo else, if not an alternative square, just leave it as white, no need to do anything
 			}
-		}
-		
+		}	
 	};
 
 	ChessBoard.prototype.colourSquare = function(colour, x, y) {
@@ -41,5 +45,62 @@ var ChessBoard = function ChessBoard(canvas) {
 	};
 };
 
-var chessBoard = new ChessBoard(document.getElementById('game'));
-chessBoard.drawBoard();
+
+
+// class ideas for now
+var ChessPiece = function ChessPiece(pathToImage, canvas) {
+	// player
+	// piece (player)
+	
+	this.canvas2 = canvas; 
+	this.context2 = this.canvas2.getContext('2d');
+	this.pieceImage = new Image();
+	this.pieceImage.src = pathToImage;
+
+
+
+	this.context2.drawImage(this.pieceImage, 10, 10);	
+
+	
+
+	// isMoveValid?
+
+	// each individual piece inherits?
+}
+
+var Square = function Square() {
+	// Piece object
+	//Square(piece)
+}
+
+var Game = function Game() {
+	// Player object, we need 2 (black and white)
+	// ChessBoard object?
+
+	// methods
+	// gameOver()
+	// getPiece()
+	// move()
+}
+
+var Render = function Render() {
+
+	Render.prototype.drawBoard = function() {
+		var chessBoard = new ChessBoard(document.getElementById('game'));
+		chessBoard.drawBoard();
+
+		var piece = new ChessPiece('img/Black B.png', document.getElementById('game'));
+		//piece.drawPiece();
+	}
+}
+
+// #5A3336
+
+// "<a href="https://commons.wikimedia.org/wiki/File:Chess_kdt45.svg#/media/File:Chess_kdt45.svg">Chess kdt45</a>" by <a href="//en.wikipedia.org/wiki/User:Cburnett" class="extiw" title="en:User:Cburnett">en:User:Cburnett</a> - <span class="int-own-work" lang="en">Own work</span><a href="//commons.wikimedia.org/wiki/File:Inkscape_Logo.svg" title="File:Inkscape Logo.svg"></a>This <a href="//en.wikipedia.org/wiki/Vector_images" class="extiw" title="w:Vector images">vector image</a> was created with <a href="//commons.wikimedia.org/wiki/Help:Inkscape" title="Help:Inkscape">Inkscape</a>.. Licensed under <a href="http://creativecommons.org/licenses/by-sa/3.0/" title="Creative Commons Attribution-Share Alike 3.0<p></p>">CC BY-SA 3.0</a> via <a href="//commons.wikimedia.org/wiki/">Wikimedia Commons</a>.
+// Chess Pieces ^^
+
+var render = new Render();
+setInterval(render.drawBoard, 100);
+render.drawBoard();
+
+}); // jQuery ready
