e81916e1fe180e9465707df47eb0a26dedef3971
Tom Kerr <tomhammers@googlemail.com>
2015-12-17 12:11:34 +0000

    Pawns can capture

--- public/js/gameLogic.js
+++ public/js/gameLogic.js
@@ -10,7 +10,11 @@ var GameLogic = function GameLogic(player, boardLayout, chessBoard) {
 	this.pieceType = "";
 	this.prevSquare; // object that was on prev square clicked
 	this.square;	// object thats on the current square clicked
-	
+	this.y;
+	this.x;
+	this.prevY;
+	this.prevX;
+
 	this.pieceMoved = false;
 
 	this.validMove = false;
@@ -26,22 +30,19 @@ GameLogic.prototype.oppenentPiece = function () {
 // used in if statement after a valid 2nd click
 // this should return true if move is valid
 GameLogic.prototype.checkMove = function () {
-	// first assign variables
+	// first assign variables with data from attempted move to make future code more readable
+	this.y = this.chessBoard.squareClickedY;
+	this.x = this.chessBoard.squareClickedX;
+	this.prevY = this.chessBoard.prevSquareClickedY;
+	this.prevX = this.chessBoard.prevSquareClickedX;
 	this.prevSquare = this.boardLayout.pieceLayout[this.chessBoard.prevSquareClickedY][this.chessBoard.prevSquareClickedX];
 	this.Square = this.boardLayout.pieceLayout[this.chessBoard.squareClickedY][this.chessBoard.squareClickedX];
-
 	this.pieceType = this.prevSquare.pieceType;
-
+	// check if piece has moved previously
 	this.hasPieceMoved();
-	// does the square clicked on have an oppenent piece?
-	// don't need to do this here in real game?
-	//this.squareHasPiece(this.chessBoard.squareClickedY, this.chessBoard.squareClickedX);
 
-	if (this.squareContainsPiece) {
-		console.log("opp piece");
 
-	}
-
+	// call the correct method depending on which piece was selected by player
 	switch (this.pieceType) {
 		case "pawn":
 			this.checkPawnMove();
@@ -57,36 +58,45 @@ GameLogic.prototype.checkPawnMove = function () {
 	// function to check if pawn can move forward legally 
 	function validPawnMoveForward(num) {
 		// if click is 1 or 2 squares forward AND identical x co-ordinates of both clicks
-		if (self.chessBoard.squareClickedY === self.chessBoard.prevSquareClickedY + num &&
-			self.chessBoard.squareClickedX === self.chessBoard.prevSquareClickedX) {
-			//this.validMove = true;
-			//return true;
+		if (self.y === self.prevY + num && self.x === self.prevX) {
+			// y = difference between squareclicked and prevSquare clicked
 			var y = 0;
-			if (num<0) { // white pieces
-				y = self.chessBoard.prevSquareClickedY - self.chessBoard.squareClickedY;
+			if (num < 0) { // white pieces
+				y = self.prevY - self.y; // 1st click - 2nd click
 			} else { // black pieces
-				y = self.chessBoard.squareClickedY - self.chessBoard.prevSquareClickedY;
+				y = self.y - self.prevY; // 2nd click - 1st click
 			}
-			
-			for(var i=0; i<y; i++) {
-				if (num<0) {
+			// looping through squares checking for pieces blocking the way
+			for (var i = 0; i < y; i++) {
+				if (num < 0) {
 					// white pawn, iterate back from square clicked to prevsquare clicked
-					this.squareHasPiece(self.chessBoard.squareClickedY + i, self.chessBoard.squareClickedX);
+					this.squareHasPiece(self.y + i, self.x);
 				} else {
 					// black pawn, iterate forward from square clicked to prevsquare clicked
-					this.squareHasPiece(self.chessBoard.squareClickedY - i, self.chessBoard.squareClickedX);
-				}	
+					this.squareHasPiece(self.y - i, self.x);
+				}
 			}
-			
+			// if no pieces in the way, its a valid move
 			if (!this.squareContainsPiece) {
 				this.validMove = true;
 			} else { // if a piece is blocking, invalid move
 				this.validMove = false;
 			}
+		}
 
-		} // if statement	
 	} // validPawnMoveForward()
 	
+	function validCapture(num) {
+		// remember already checked 2nd click is not on own piece so only need to check !== null
+		if (self.y === self.prevY + num && self.x === self.prevX + num || self.x === self.prevX - num) {
+			this.squareHasPiece(self.y, self.x);
+
+			if (this.squareContainsPiece) {
+				this.validMove = true;
+			}
+		}
+	}
+
 	var validForwardSquares = [];
 	if (this.player.colourPieces === "white") {
 		validForwardSquares = [-1, -2];
@@ -96,7 +106,9 @@ GameLogic.prototype.checkPawnMove = function () {
 	}
 
 	var self = this;
-
+	// checking for a valid capture
+	validCapture.call(self, validForwardSquares[0]);
+	// checking for valid move forward
 	if (this.pieceMoved) { // if pawn has moved before, can only move one square forward
 		// call allows to set the context in the function of 'this'
 		validPawnMoveForward.call(self, validForwardSquares[0]);
@@ -105,7 +117,9 @@ GameLogic.prototype.checkPawnMove = function () {
 		validPawnMoveForward.call(self, validForwardSquares[0]);
 		validPawnMoveForward.call(self, validForwardSquares[1]);
 	}
-}
+
+
+} // checkPawnMove()
 
 // method to check if square has a piece
 // already checked user hasn't clicked on own piece on second click
@@ -133,7 +147,7 @@ GameLogic.prototype.movePiece = function () {
 	this.boardLayout.pieceLayout[this.chessBoard.prevSquareClickedY][this.chessBoard.prevSquareClickedX] = null;
 	// move temp object to the new square clicked
 	this.boardLayout.pieceLayout[this.chessBoard.squareClickedY][this.chessBoard.squareClickedX] = tempObjPlaceHolder;
-	
+
 
 	this.validMove = false;
 }
